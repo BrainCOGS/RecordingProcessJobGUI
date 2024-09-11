df6749c9a681a8ef6d0451046ed37c49f655e121
Christian Tabedzki <35670232+tabedzki@users.noreply.github.com>
2024-09-11 11:21:31 -0400

    Updating the conda detecting

--- @RecordingProcessJobGUI/getPythonEnv.m
+++ @RecordingProcessJobGUI/getPythonEnv.m
@@ -4,7 +4,10 @@ function getPythonEnv(app)
 try
     if ismac
         this_path = getenv('PATH');
-        this_path = [this_path ':/Users/alvaros/opt/anaconda3/condabin'];
+        home = getenv('HOME');
+        this_path = [this_path ':' home '/opt/anaconda3/condabin'];
+        this_path = [this_path ':' home '/conda_env_txt/y'];
+        this_path = [this_path ':' home '/bin'];
         setenv('PATH', this_path);
     end
 
@@ -12,11 +15,24 @@ try
         % (py_env_name, py_iblenv_name)
         [~, conda_envs] = system('conda env list');
 
-        idx_py_env = strfind(conda_envs, RecordingProcessJobGUI.py_env_name);
-        
-        app.py_env = strtrim(conda_envs(idx_py_env(1)+length(RecordingProcessJobGUI.py_env_name): ...
-            idx_py_env(2)+length(RecordingProcessJobGUI.py_env_name)));
-        
+        % Split the input string into lines
+        lines = strsplit(conda_envs, '\n');
+
+        % Initialize the variable to store the matching path
+        matching_path = '';
+
+        % Loop through each line to find the matching environment
+        for i = 1:length(lines)
+            line = strtrim(lines{i});
+            if contains(line, RecordingProcessJobGUI.py_env_name)
+                matching_path = strsplit(line, ' ');
+                matching_path = matching_path{length(matching_path)};
+                break;
+            end
+        end
+
+        app.py_env = matching_path;
+
         if ispc
             app.py_env = fullfile( app.py_env, 'python');
             %Surround with double quotes for filepaths with spaces
@@ -24,23 +40,30 @@ try
         else
             app.py_env = fullfile( app.py_env, 'bin', 'python');
         end
-        
-        idx_py_iblenv = strfind(conda_envs, RecordingProcessJobGUI.py_iblenv_name);
-        
-        app.py_ibl_env = strtrim(conda_envs(idx_py_iblenv(1)+length(RecordingProcessJobGUI.py_env_name): ...
-            idx_py_iblenv(2)+length(RecordingProcessJobGUI.py_iblenv_name)));
+
+        % Loop through each line to find the matching environment
+        for i = 1:length(lines)
+            line = strtrim(lines{i});
+            if contains(line, RecordingProcessJobGUI.py_iblenv_name)
+                matching_path = strsplit(line, ' ');
+                matching_path = matching_path{length(matching_path)};
+                break;
+            end
+        end
+
+        app.py_ibl_env = matching_path;
 
         if ispc
             app.py_ibl_env = fullfile( app.py_ibl_env, 'python');
         else
             app.py_ibl_env = fullfile( app.py_ibl_env, 'bin', 'python');
         end
-        
+
         %Surround with double quotes for filepaths with spaces
         app.py_ibl_env = ['"' app.py_ibl_env '"'];
-        
-        
-        
+
+
+
         app.py_enabled = true;
 catch
     app.py_env = [];
